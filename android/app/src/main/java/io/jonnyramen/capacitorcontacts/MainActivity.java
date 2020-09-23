0d5eedfbbfff65af46b21cbecdac978ad320e771
mc-perry <jonathan@m-craft.com>
2020-09-23 13:03:52 +0900

    Renamed project and finished tutorial

--- android/app/src/main/java/io/jonnyramen/capacitorcontacts/MainActivity.java
+++ android/app/src/main/java/io/jonnyramen/capacitorcontacts/MainActivity.java
@@ -1,4 +1,4 @@
-package io.ionic.starter;
+package io.jonnyramen.capacitorcontacts;
 
 import android.os.Bundle;
 
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,43 @@
 import { Component } from '@angular/core';
 
+import { Contact } from '@capacitor-community/contacts';
+
+import { Plugins } from '@capacitor/core';
+const { Contacts } = Plugins;
+
+import { CallNumber } from '@ionic-native/call-number/ngx';
+import { SMS } from '@ionic-native/sms/ngx';
+import { isPlatform } from '@ionic/angular';
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  styleUrls: ['home.page.scss']
 })
 export class HomePage {
+  contacts = [];
+  constructor(private callNumber: CallNumber, private sms: SMS) {
+    this.loadContacts();
+  }
 
-  constructor() {}
+  async loadContacts() {
+    if (isPlatform('android')) {
+      let permission = await Contacts.getPermissions();
+      if (!permission.granted) {
+        return;
+      }
+    }
+    Contacts.getContacts().then(result => {
+      console.log(result);
+      this.contacts = result.contacts;
+    });
+  }
 
+  call(contact: Contact) {
+    this.callNumber.callNumber(contact.phoneNumbers[0], true);
+  }
+
+  sendSms(contact: Contact) {
+    this.sms.send(contact.phoneNumbers[0], 'This is my pred');
+  }
 }
